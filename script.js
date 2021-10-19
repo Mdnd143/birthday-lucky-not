ad2035c0dcaadec88b6225a704add6af12170bb3
Mohd Nadeem <90819097+Mdnd143@users.noreply.github.com>
2021-10-19 05:18:48 +0000

    js code updated and minor changes in css

--- script.js
+++ script.js
@@ -8,12 +8,17 @@ function checkBirthdateIsLucky(){
   compareValues(sum,luckyNumber.value)
 }
 function compareValues(sum,luckyNumber){
+  if(sum>0 && luckyNumber>0){
  if(sum%luckyNumber===0){
    outputBox.innerText="Your Birthdate is lucky";
  }
  else{
    outputBox.innerText="Your Birthdate is not lucky";
  }
+}
+else{
+  outputBox.innerText="either you entered invalid date/number or you have not entered any date/number.";
+}
 }
 function calculateSum(dob){
  dob=dob.replaceAll("-","");
